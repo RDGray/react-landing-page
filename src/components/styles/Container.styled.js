ab3e41e10b6b38991351c3673cf3ba5bcc74c31f
RDGray <70549101+RDGray@users.noreply.github.com>
2022-01-08 19:47:46 +0300

    Init react landing page

---
+++ src/components/styles/Container.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  //place for css
+
+  width: 1000px;
+  max-width: 100%;
+  padding: 0 20px;
+  margin: 0 auto;
+`;
